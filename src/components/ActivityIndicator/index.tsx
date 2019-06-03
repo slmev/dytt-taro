dd455fdf43b72f47edd99b718e6b14e6513f1577
neo <zhujie1993@hotmail.com>
2019-06-03 11:18:00 +0800

    loadingIcon

--- src/components/ActivityIndicator/index.tsx
+++ src/components/ActivityIndicator/index.tsx
@@ -1,12 +1,14 @@
 import Taro from '@tarojs/taro';
 import { View } from '@tarojs/components';
 
+import { hexToRgb } from '../../utils/utils'
 import './index'
 
-function Loading({size, color}) {
+function ActivityIndicator({ size, color }) {
+  const rgb = hexToRgb(color);
   return (
-    <View className='content' style={{ width: size, height: size,  }} />
+    <View className='content' style={{ width: size, height: size, borderColor: `rgba(${rgb}, 0.2)`, borderTopColor: color  }} />
   )
 }
 
-export default Loading
+export default ActivityIndicator
