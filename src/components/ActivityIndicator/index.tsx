30320913f6b1398c8d5be7b6af293e22bb8ca5ee
neo <zhujie1993@hotmail.com>
2019-06-05 17:34:22 +0800

    修复样式没有效果，loading无法显示的问题

--- src/components/ActivityIndicator/index.tsx
+++ src/components/ActivityIndicator/index.tsx
@@ -2,12 +2,12 @@ import Taro from '@tarojs/taro';
 import { View } from '@tarojs/components';
 
 import { hexToRgb } from '../../utils/utils'
-import './index'
+import './index.less'
 
 function ActivityIndicator({ size, color }) {
   const rgb = hexToRgb(color);
   return (
-    <View className='content' style={{ width: size, height: size, borderColor: `rgba(${rgb}, 0.2)`, borderTopColor: color  }} />
+    <View className='content' style={{ width: `${size}px`, height: `${size}px`, borderColor: `rgba(${rgb}, 0.2)`, borderTopColor: color  }} />
   )
 }
 
